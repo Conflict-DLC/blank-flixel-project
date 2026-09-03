d1b58bd0c817fada8df792c71828133ec01cd125
aleks <thatbnuuyalex@gmail.com>
2026-09-03 12:44:18 +0200

    16:9 because i'm not using 4:3

--- source/Main.hx
+++ source/Main.hx
@@ -5,9 +5,19 @@ import openfl.display.Sprite;
 
 class Main extends Sprite
 {
+	public static final game = {
+		width: 1280,
+		height: 720,
+		initialState: PlayState,
+		framerate: 60,
+		skipSplash: true,
+		startFullscreen: false
+	};
+
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, PlayState));
+
+		addChild(new FlxGame(game.width, game.height, game.initialState, game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
 	}
 }
